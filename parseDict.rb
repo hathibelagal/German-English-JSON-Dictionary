c3e495175d86f6680b23dc45cf5c4f4e2fd0c5c9
Hathibelagal <mlrdae@gmail.com>
2015-10-03 18:01:19 +0530

    English-German mode

--- parseDict.rb
+++ parseDict.rb
@@ -4,6 +4,8 @@ require 'nokogiri'
 require 'open-uri'
 require 'json'
 
+mode=ARGV[0]    # Should be "ge" for German to English
+
 dictionaryHTML = Nokogiri::HTML(
 	open("http://www.gutenberg.org/files/3212/3212-h/3212-h.htm")
 )
@@ -14,7 +16,11 @@ dictionaryHTML.xpath('//tr').each do |row|
     if row.children.size() == 2 then
         german = row.children[0].content
         english = row.children[1].content
-        dictionaryObject[german]=english
+        if mode=="ge" then
+            dictionaryObject[german]=english
+        elsif mode=="eg" then
+            dictionaryObject[english]=german
+        end
     end
 end
 
